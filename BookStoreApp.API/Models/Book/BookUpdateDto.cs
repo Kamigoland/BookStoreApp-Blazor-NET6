50a15512721e4e88bf661754e90cd46842b645fe
amadeusz.idziak <amadeusz.idziak@PC208>
2022-12-05 14:30:44 +0100

    Completed Book API Setup

---
+++ BookStoreApp.API/Models/Book/BookUpdateDto.cs
@@ -0,0 +1,11 @@
+﻿namespace BookStoreApp.API.Models.Book
+{
+    public class BookUpdateDto : BaseDto
+    {
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public string Summary { get; set; }
+        public string Image { get; set; }
+        public decimal Price { get; set; }
+    }
+}
